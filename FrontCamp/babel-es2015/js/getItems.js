7fa67917f2c5031ed39a739bd0c2e9ea053b6147
yana95 <yanazaiceva0602@gmail.com>
2017-12-04 21:13:39 +0300

    make it work for ie10

--- FrontCamp/babel-es2015/js/getItems.js
+++ FrontCamp/babel-es2015/js/getItems.js
@@ -1,6 +1,7 @@
 /**
  * Created by Yana_Zaitsava on 11/26/2017.
  */
+ import 'whatwg-fetch';
 
 module.exports = (url) => {
     const promise = fetch(url);    //Using ES6 fetch
--- FrontCamp/babel-es2015/js/index.js
+++ FrontCamp/babel-es2015/js/index.js
@@ -1,2 +1,3 @@
+import "babel-polyfill";
 import App from './App.js';
 new App();

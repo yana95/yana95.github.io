040aa5b0b6dc980a953f54e01a1ca389357fab56
Yana_Zaitsava <Zaitsava_Yana@epam.com>
2017-12-10 17:54:51 +0300

    webpack

---
+++ FrontCamp/webpack/js/getItems.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Yana_Zaitsava on 11/26/2017.
+ */
+ import 'whatwg-fetch';
+
+const getItems = (url) => {
+    const promise = fetch(url);    //Using ES6 fetch
+    return promise.then((res) => res.json()); //Using ES6 promise
+}
+
+export default getItems;
---
+++ FrontCamp/webpack/webpack.config.dev.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+module.exports = {
+    entry: {
+        app: './js/index.js',
+        styles: './scss/_styles.scss'
+    },
+    output: {
+        filename: '[name]-build.js',
+        path: path.resolve( __dirname, "dist")
+    },
+    devServer: {
+        contentBase: './'
+    },
+    devtool: 'eval',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.png$/,
+                loader: "file-loader",
+                options: {
+                    name: '[path][name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Test',
+            hash: true,
+            template: './index.html'
+        }),
+        new ExtractTextPlugin("styles.css"),
+    ]
+};
